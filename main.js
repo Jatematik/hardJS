31d7b85003bedf21aefca90f045ae9525afde3f7
Jatematik <freerun_dimon@mail.ru>
2020-11-22 20:32:58 +0600

    feat: lesson5

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 'use strict';
 
-const foo = (arg) => {
-    if (arg !== String(arg)) {
-        alert('В функцию передана не строка!');
-        return;
-    }
-    let a = arg.trim();
-    if (a.length > 30) {
-        a = a.substring(0, 30) + '...';
-    }
-    console.log(a);
-    console.log(a.length);
-};
+let arr =['2564', '48975', '132178', '97456', '23579', '9875', '356'];
 
-foo('    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae delectus quasi quisquam deleniti dolor cum quaerat ut aliquid debitis voluptatibus, officia quidem illo odit, dicta aut eius harum temporibus impedit.  ');
+arr.forEach((item) => {
+    if (item[0] === '2' || item[0] === '4') {
+        console.log(item);
+    }
+});
+
+
+// for (let i = 0; i <=100; i++) {
+
+// }
+
+loop:
+for (let i = 2; i <= 100; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+        continue loop;
+    }
+  }
+  console.log(i + ` : Делители этого числа: 1 и ${i}`);
+}
