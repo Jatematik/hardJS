3ada10a712d5998c4c7bc5b302fb861370482c3c
Jatematik <freerun_dimon@mail.ru>
2020-11-22 23:03:32 +0600

    lesson7

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 'use strict';
 
-let arr =['2564', '48975', '132178', '97456', '23579', '9875', '356'];
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+const date = new Date();
+const weekDay = date.getDay();
+ul.style.cssText = `position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+font-size: 26px;
+font-family: sans-serif;
+list-style-type: none;
+margin: 0;
+padding: 0;
+text-align: center;
+`;
+const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
 
-arr.forEach((item) => {
-    if (item[0] === '2' || item[0] === '4') {
-        console.log(item);
+document.body.appendChild(ul);
+for (let i = 0; i < 7; i++) {
+  const cloneLi = li.cloneNode();
+  cloneLi.style.cssText = `margin: 5px 0;`;
+  ul.append(cloneLi);
+  cloneLi.textContent = week[i];
+}
+
+const cloneLi = document.querySelectorAll('li');
+
+cloneLi.forEach((item, index) => {
+    if (index === 5 || index === 6) {
+      item.style.fontStyle = 'italic';
+    }
+    if (weekDay - 1 === index) {
+      item.style.fontWeight = 'bold';
+    } else if (index === 6 && weekDay === 0) {
+      item.style.fontWeight = 'bold';
     }
 });
-
-
-// for (let i = 0; i <=100; i++) {
-
-// }
-
-loop:
-for (let i = 2; i <= 100; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j === 0) {
-        continue loop;
-    }
-  }
-  console.log(i + ` : Делители этого числа: 1 и ${i}`);
-}
